3051efbb0046dc22b29e935f191a730b855cb8c8
Yukesh Dhakal <yukeshdhakal42@gmail.com>
2024-07-03 15:34:10 +0545

    Stop tracking .env file and add new api endpoints

---
+++ app/api/blog/route.js
@@ -0,0 +1,43 @@
+import { connectDB } from "@/lib/config/db";
+import BlogModel from "@/lib/models/BlogModel";
+const { NextResponse } = require("next/server");
+import { writeFile } from "fs/promises";
+
+const LoadDB = async () => {
+  await connectDB();
+};
+
+LoadDB();
+
+export async function GET(request) {
+  return NextResponse.json({ msg: "API is working!" });
+}
+
+export async function POST(request) {
+  const formData = await request.formData();
+  const timestamp = Date.now();
+
+  const image = formData.get("image");
+  const imageByteData = await image.arrayBuffer();
+  const buffer = Buffer.from(imageByteData);
+  const path = `./public/${timestamp}_${image.name}`;
+
+  // Save image in the public folder
+  await writeFile(path, buffer);
+  const imgUrl = `/${timestamp}_${image.name}`;
+    
+  // Save other data in the database
+  const blogData = {
+    title: `${formData.get("title")}`,
+    description: `${formData.get("description")}`,
+    category: `${formData.get("category")}`,
+    author: `${formData.get("author")}`,
+    image: `${imgUrl}`,
+    authorImg: `${formData.get("authorImg")}`
+  }
+
+  await BlogModel.create(blogData);
+  console.log("Blog saved!");
+
+  return NextResponse.json({ success: true, msg: "Blog added!" });
+}
